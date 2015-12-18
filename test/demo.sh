bb44efa71cad5dc696e0fd1ca3ad1255bc9ecda6
Albert Sun <Albert.Sun@nytimes.com>
2015-12-18 17:00:05 -0500

    add static example

--- test/demo.sh
+++ test/demo.sh
@@ -2,5 +2,10 @@
 
 go run test/server/server.go & go run kubernetes-dns-reverse-proxy.go \
                                        --kubernetes-dns-domain=127.0.0.1.xip.io:8090 \
+                                       --domain-suffixes=.pub.stg.127.0.0.1.xip.io:8080 \
+                                       --static-host=int-static-prd.s3-website-us-east-1.amazonaws.com \
+                                       --static \
+                                       --static-scheme=http \
+                                       --static-path=/ \
                                        --routes test/routes.json \
-                                       --domain-suffixes=.pub.stg.127.0.0.1.xip.io:8080
+                                       
